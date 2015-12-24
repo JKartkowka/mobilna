64e2cdae4cdef88636d9e8a4af4d3abd2a93db4d
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-12-24 18:04:57 +0100

    Navigated to AuthenticationActivity

--- app/src/main/java/jkartkowka/jkartkwkamobile/GroupsListWireframe.java
+++ app/src/main/java/jkartkowka/jkartkwkamobile/GroupsListWireframe.java
@@ -1,10 +1,19 @@
 package jkartkowka.jkartkwkamobile;
 
+import android.content.Intent;
+
 /**
  * Created by marian on 19.12.2015.
  */
 public class GroupsListWireframe extends JKWireframe {
     public GroupsListWireframe(JKActivity activity) {
         super(activity);
+    }
+
+    public void navigateToAuthentication(int testId, int groupId) {
+        Intent intent = new Intent(activity, AuthenticationActivity.class);
+        intent.putExtra("testID", testId);
+        intent.putExtra("groupID", groupId);
+        activity.startActivity(intent);
     }
 }
