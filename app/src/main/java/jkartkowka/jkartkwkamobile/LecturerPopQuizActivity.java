85dc413cc571c9b3e06f9af71aeaa046c7513dcd
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-12-29 20:36:32 +0100

    Added interaction and wireframe

--- app/src/main/java/jkartkowka/jkartkwkamobile/LecturerPopQuizActivity.java
+++ app/src/main/java/jkartkowka/jkartkwkamobile/LecturerPopQuizActivity.java
@@ -2,12 +2,19 @@ package jkartkowka.jkartkwkamobile;
 
 import android.os.Bundle;
 
+import jkartkowka.jkartkwkamobile.network.RequestSender;
+
 public class LecturerPopQuizActivity extends JKActivity {
+
+    private LecturerPopQuizWireframe wireframe;
+    private LecturerPopQuizInteractor interactor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer_pop_quiz);
+        wireframe = new LecturerPopQuizWireframe(this);
+        interactor = new LecturerPopQuizInteractor(new RequestSender(getApplicationContext()));
     }
 
 }
---
+++ app/src/main/java/jkartkowka/jkartkwkamobile/LecturerPopQuizInteractor.java
@@ -0,0 +1,13 @@
+package jkartkowka.jkartkwkamobile;
+
+import jkartkowka.jkartkwkamobile.network.RequestSender;
+
+/**
+ * Created by marian on 29.12.2015.
+ */
+public class LecturerPopQuizInteractor extends JKInteractor {
+
+    public LecturerPopQuizInteractor(RequestSender requestSender) {
+        super(requestSender);
+    }
+}
