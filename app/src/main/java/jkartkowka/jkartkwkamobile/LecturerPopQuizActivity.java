63a621f38a2bd3892a6e012c7938abed871e3347
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-12-29 20:32:28 +0100

    Added LecturerPopQuizActivity

---
+++ app/src/main/java/jkartkowka/jkartkwkamobile/LecturerPopQuizActivity.java
@@ -0,0 +1,13 @@
+package jkartkowka.jkartkwkamobile;
+
+import android.os.Bundle;
+
+public class LecturerPopQuizActivity extends JKActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecturer_pop_quiz);
+    }
+
+}
